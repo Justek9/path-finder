56916fc05856e62ddbd91325b294215c49e454cf
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-06-21 13:23:39 +0200

    draw grid and assign attributes to cells

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,24 @@
 const app = {
-	initGrid: function () {
-		const thisApp = this
+  initGrid: function () {
+    // const thisApp = this;
+    let gridContainer = document.querySelector('.container');
 
-		const gridEl = document.querySelector('.test')
+    // create grid and add datasets attributes
+    for (let rows = 1; rows <= 10; rows++) {
+      for (let columns = 1; columns <= 10; columns++) {
+        const cell = document.createElement('div');
+        cell.classList.add('row');
+        cell.setAttribute('data-row', `${rows}`);
+        cell.setAttribute('data-column', `${columns}`);
+        gridContainer.appendChild(cell);
+      }
+    }
+    console.log(gridContainer);
+  },
 
-		gridEl.addEventListener('click', function () {
-			console.log('test')
-		})
-	},
-
-	init: function () {
-		const thisApp = this
-		thisApp.initGrid()
-	},
-}
-app.init()
+  init: function () {
+    const thisApp = this;
+    thisApp.initGrid();
+  },
+};
+app.init();
