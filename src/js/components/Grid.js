031a0491bf12dca80dd8181e5ca75db05baac448
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-06-26 18:31:51 +0200

    add event listeners on buttons, update manageGrid and updateUI func

--- src/js/components/Grid.js
+++ src/js/components/Grid.js
@@ -1,134 +1,213 @@
-import { select, classNames } from '../settings.js';
+import { select, classNames } from '../settings.js'
 
 class Grid {
-  constructor() {
-    const thisGrid = this;
-    thisGrid.getData();
-    thisGrid.isValidAsNeighbour();
-    thisGrid.updateUI();
-    thisGrid.drawGrid();
-    thisGrid.manageGrid();
-  }
+	constructor() {
+		const thisGrid = this
+		thisGrid.getData()
+		thisGrid.isValidAsNeighbour()
+		thisGrid.updateUI()
+		thisGrid.drawGrid()
+		thisGrid.manageGrid()
+		thisGrid.finishDrawingBtnHandler()
+		thisGrid.computeBtnHandler()
+		thisGrid.startAgainBtnHandler()
+	}
 
-  getData() {
-    const thisGrid = this;
-    thisGrid.gridContainer = document.querySelector(select.containerOf.grid);
-    thisGrid.state = [];
-    thisGrid.selected = [];
-    thisGrid.neighbours = [];
-    thisGrid.isFirstClick = true;
-  }
+	getData() {
+		const thisGrid = this
+		thisGrid.gridContainer = document.querySelector(select.containerOf.grid)
+		thisGrid.state = []
+		thisGrid.selected = []
+		thisGrid.neighbours = []
+		thisGrid.startPoint = []
+		thisGrid.finishPoint = []
+		thisGrid.isFirstClick = true
+		thisGrid.isFinishedDrawing = false
+		thisGrid.isStartSelected = false
+		thisGrid.isFinishSelected = false
+		thisGrid.finishDrawingBtn = document.querySelector(select.button.finish)
+		thisGrid.computeRouteBtn = document.querySelector(select.button.compute)
+		thisGrid.startAgainBtn = document.querySelector(select.button.startAgain)
+		thisGrid.instructions = document.querySelector(select.element.draw)
+	}
 
-  updateUI(row, col) {
-    const thisGrid = this;
-    let allTiles = document.querySelectorAll('.tile');
+	updateUI(row, col) {
+		const thisGrid = this
+		let allTiles = document.querySelectorAll('.tile')
 
-    for (let tile of allTiles) {
-      thisGrid.state[row][col] === 0 ? tile.classList.remove(classNames.tile.selected) : '';
+		for (let tile of allTiles) {
+			thisGrid.state[row][col] === 0 ? tile.classList.remove(classNames.tile.selected) : ''
 
-      thisGrid.neighbours.forEach(el => {
-        if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
-          tile.classList.add(classNames.tile.neighbour);
-        }
-      });
+			thisGrid.startPoint.forEach(el => {
+				if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
+					tile.classList.remove(classNames.tile.selected)
+					tile.classList.add(classNames.tile.start)
+				}
+			})
 
-      thisGrid.selected.forEach(el => {
-        if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
-          tile.classList.remove(classNames.tile.neighbour);
-          tile.classList.add(classNames.tile.selected);
-        }
-      });
-    }
-  }
+			thisGrid.finishPoint.forEach(el => {
+				if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
+					tile.classList.remove(classNames.tile.selected)
+					tile.classList.add(classNames.tile.finish)
+				}
+			})
 
-  gridState() {
-    const thisGrid = this;
-    // States:
-    // 0 - initial state after loading for all tiles
-    // 1 - selected element
-    // 2 - neighbour of selected element
+			thisGrid.neighbours.forEach(el => {
+				if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
+					tile.classList.add(classNames.tile.neighbour)
+				}
+			})
 
-    for (let i = 0; i < 10; i++) {
-      thisGrid.state.push(new Array(10).fill(0));
-    }
-  }
+			thisGrid.selected.forEach(el => {
+				if (+tile.dataset.row === el[0] && +tile.dataset.column === el[1]) {
+					tile.classList.remove(classNames.tile.neighbour)
+					tile.classList.add(classNames.tile.selected)
+				}
+			})
+		}
+	}
 
-  drawGrid() {
-    const thisGrid = this;
+	gridState() {
+		const thisGrid = this
+		// States:
+		// 0 - initial state after loading for all tiles
+		// 1 - selected element
+		// 2 - neighbour of selected element
+		// 3 - start point to compute route
+		// 4 - end point to compute route
 
-    // create grid and add datasets attributes
-    for (let rows = 0; rows < 10; rows++) {
-      for (let columns = 0; columns < 10; columns++) {
-        const cell = document.createElement('div');
-        cell.classList.add(classNames.tile.tile);
-        cell.setAttribute('data-row', rows);
-        cell.setAttribute('data-column', columns);
-        thisGrid.gridContainer.appendChild(cell);
-      }
-    }
-    thisGrid.gridState();
-  }
+		for (let i = 0; i < 10; i++) {
+			thisGrid.state.push(new Array(10).fill(0))
+		}
+	}
 
-  manageGrid() {
-    const thisGrid = this;
-    // add event listener on cells
-    thisGrid.gridContainer.addEventListener('click', function (event) {
-      // get data of selected cell
-      let row = Number(event.target.dataset.row);
-      let column = Number(event.target.dataset.column);
+	drawGrid() {
+		const thisGrid = this
 
-      // Set grid state to 0, 1 or 2
-      if (thisGrid.state[row][column] === 2) {
-        thisGrid.state[row][column] = 1;
-        let index = thisGrid.neighbours.indexOf([row, column]);
-        thisGrid.neighbours.splice(index, 1);
-        thisGrid.setAsSelected(row, column);
-      } else if (thisGrid.isFirstClick && thisGrid.state[row][column] === 0) {
-        thisGrid.state[row][column] = 1;
-        thisGrid.setAsSelected(row, column);
-      } else if (!thisGrid.isFirstClick && thisGrid.state[row][column] === 0)
-        alert('Please choose correct tile (green bakground)');
+		// create grid and add datasets attributes
+		for (let rows = 0; rows < 10; rows++) {
+			for (let columns = 0; columns < 10; columns++) {
+				const cell = document.createElement('div')
+				cell.classList.add(classNames.tile.tile)
+				cell.setAttribute('data-row', rows)
+				cell.setAttribute('data-column', columns)
+				thisGrid.gridContainer.appendChild(cell)
+			}
+		}
+		thisGrid.gridState()
+	}
 
-      thisGrid.updateUI(row, column);
-      thisGrid.isFirstClick = false;
-    });
-  }
+	manageGrid() {
+		const thisGrid = this
 
-  setAsSelected(row, column) {
-    const thisGrid = this;
-    thisGrid.selected.push([row, column]);
+		// add event listener on cells
+		thisGrid.gridContainer.addEventListener('click', function (event) {
+			// get data of selected cell
+			let row = Number(event.target.dataset.row)
+			let column = Number(event.target.dataset.column)
 
-    // check: 1) if neighbour is within grid border, 2) if it is not already a naighbour, 3) is not selected
-    if (!(row - 1 < 0) && thisGrid.isValidAsNeighbour(row - 1, column)) {
-      thisGrid.neighbours.push([row - 1, column]);
-      thisGrid.state[row - 1][column] = 2;
-    }
-    if (!(row + 1 > 9) && thisGrid.isValidAsNeighbour(row + 1, column)) {
-      thisGrid.neighbours.push([row + 1, column]);
-      thisGrid.state[row + 1][column] = 2;
-    }
-    if (!(column - 1 < 0) && thisGrid.isValidAsNeighbour(row, column - 1)) {
-      thisGrid.neighbours.push([row, column - 1]);
-      thisGrid.state[row][column - 1] = 2;
-    }
-    if (!(column + 1 > 9) && thisGrid.isValidAsNeighbour(row, column + 1)) {
-      thisGrid.neighbours.push([row, column + 1]);
-      thisGrid.state[row][column + 1] = 2;
-    }
-    console.log(thisGrid.state);
-  }
+			// guard clauses
+			if (thisGrid.isStartSelected === true && thisGrid.isFinishSelected === true) return
+			if (thisGrid.isFinishedDrawing === true && thisGrid.state[row][column] !== 1) {
+				return
+			}
+			// Set grid state to 0, 1, 2, 3, 4
 
-  isValidAsNeighbour(r, c) {
-    const thisGrid = this;
-    if (
-      thisGrid.neighbours.filter(el => {
-        return el[0] === r && el[1] === c;
-      }).length === 0 &&
+			if (
+				thisGrid.isFinishedDrawing === true &&
+				thisGrid.state[row][column] === 1 &&
+				thisGrid.isStartSelected === false
+			) {
+				thisGrid.state[row][column] = 3
+				thisGrid.isStartSelected = true
+				thisGrid.startPoint.push([row, column])
+			}
+
+			if (thisGrid.state[row][column] === 1 && thisGrid.isStartSelected == true) {
+				thisGrid.state[row][column] = 4
+				thisGrid.isFinishSelected = true
+				thisGrid.finishPoint.push([row, column])
+				console.log(thisGrid.finishPoint, thisGrid.startPoint)
+			}
+
+			if (thisGrid.state[row][column] === 2) {
+				thisGrid.state[row][column] = 1
+				let index = thisGrid.neighbours.indexOf([row, column])
+				thisGrid.neighbours.splice(index, 1)
+				thisGrid.setAsSelected(row, column)
+			} else if (thisGrid.isFirstClick && thisGrid.state[row][column] === 0) {
+				thisGrid.state[row][column] = 1
+				thisGrid.setAsSelected(row, column)
+			} else if (!thisGrid.isFirstClick && thisGrid.state[row][column] === 0)
+				alert('Please choose correct tile (green bakground)')
+
+			thisGrid.updateUI(row, column)
+			thisGrid.isFirstClick = false
+		})
+	}
+
+	setAsSelected(row, column) {
+		const thisGrid = this
+		thisGrid.selected.push([row, column])
+
+		// check: 1) if neighbour is within grid border, 2) if it is not already a naighbour, 3) is not selected
+		if (!(row - 1 < 0) && thisGrid.isValidAsNeighbour(row - 1, column)) {
+			thisGrid.neighbours.push([row - 1, column])
+			thisGrid.state[row - 1][column] = 2
+		}
+		if (!(row + 1 > 9) && thisGrid.isValidAsNeighbour(row + 1, column)) {
+			thisGrid.neighbours.push([row + 1, column])
+			thisGrid.state[row + 1][column] = 2
+		}
+		if (!(column - 1 < 0) && thisGrid.isValidAsNeighbour(row, column - 1)) {
+			thisGrid.neighbours.push([row, column - 1])
+			thisGrid.state[row][column - 1] = 2
+		}
+		if (!(column + 1 > 9) && thisGrid.isValidAsNeighbour(row, column + 1)) {
+			thisGrid.neighbours.push([row, column + 1])
+			thisGrid.state[row][column + 1] = 2
+		}
+	}
+
+	isValidAsNeighbour(r, c) {
+		const thisGrid = this
+		if (
+			thisGrid.neighbours.filter(el => {
+				return el[0] === r && el[1] === c
+			}).length === 0 &&
 			thisGrid.selected.filter(el => {
-			  return el[0] === r && el[1] === c;
+				return el[0] === r && el[1] === c
 			}).length === 0
-    )
-      return true;
-  }
+		)
+			return true
+	}
+
+	finishDrawingBtnHandler() {
+		const thisGrid = this
+		thisGrid.finishDrawingBtn.addEventListener('click', function () {
+			thisGrid.isFinishedDrawing = true
+			console.log(thisGrid.isFinishedDrawing)
+			thisGrid.instructions.innerHTML = 'Pick start and finish'
+			thisGrid.finishDrawingBtn.classList.remove('active-btn')
+			thisGrid.computeRouteBtn.classList.add('active-btn')
+		})
+	}
+
+	computeBtnHandler() {
+		const thisGrid = this
+		thisGrid.computeRouteBtn.addEventListener('click', function () {
+			thisGrid.instructions.innerHTML = 'The best route is...'
+			thisGrid.computeRouteBtn.classList.remove('active-btn')
+			thisGrid.startAgainBtn.classList.add('active-btn')
+		})
+	}
+
+	startAgainBtnHandler() {
+		const thisGrid = this
+		thisGrid.startAgainBtn.addEventListener('click', function () {
+			thisGrid.startAgainBtn.classList.remove('active-btn')
+			thisGrid.finishDrawingBtn.classList.add('active-btn')
+		})
+	}
 }
-export default Grid;
+export default Grid
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,28 +1,37 @@
 export const select = {
-  containerOf: {
-    grid: '.grid',
-    pages: '#pages',
-  },
+	containerOf: {
+		grid: '.grid',
+		pages: '#pages',
+	},
 
-  element: {
-    tiles: '.tile',
-  },
+	element: {
+		tiles: '.tile',
+		draw: '.draw',
+	},
 
-  nav: {
-    links: '.main-nav a',
-  },
-};
+	nav: {
+		links: '.main-nav a',
+	},
+
+	button: {
+		finish: '.finish',
+		compute: '.compute',
+		startAgain: '.start',
+	},
+}
 
 export const classNames = {
-  pages: {
-    active: 'active',
-  },
-  nav: {
-    active: 'active',
-  },
-  tile: {
-    tile: 'tile',
-    selected: 'selected',
-    neighbour: 'neighbour',
-  },
-};
+	pages: {
+		active: 'active',
+	},
+	nav: {
+		active: 'active',
+	},
+	tile: {
+		tile: 'tile',
+		selected: 'selected',
+		neighbour: 'neighbour',
+		start: 'first-selected',
+		finish: 'last-selected',
+	},
+}
